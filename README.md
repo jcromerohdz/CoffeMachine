67a835bdb2888a3269f25c513e364fe16478cfdf
ITT <jcromerohdz@gmail.com>
2025-05-21 14:04:14 -0700

    Completed the cofeeApp

--- README.md
+++ README.md
@@ -30,3 +30,25 @@
 	b. Recuerde que las monedas de 25 centavos = $0.25, las de 10 centavos = 	$0.10, las de 5 centavos = $0.05, las de un centavo = $0.01.
 
 	c. Calcule el valor monetario de las monedas insertadas. Por ejemplo, 1 	moneda de 25 centavos, 2 de 10 centavos, 1 de 5 centavos, 2 de un centavo = 	0.25 + 0.1 x 2 + 0.05 + 0.01 x 2 = $0.52.
+
+6. ¿Verificar si la transacción fue exitosa?
+    a. Verificar que el usuario haya ingresado suficiente dinero para comprar la bebida seleccionada. Por ejemplo, un café con leche cuesta $2.50, pero solo se ingresaron $0.52. Después de contar las monedas, el programa debería decir "Lo siento, no hay suficiente dinero. Se reembolsó el dinero".
+    b. Si el usuario ha ingresado suficiente dinero, el costo de la bebida se suma a la máquina como ganancia y esto se reflejará la próxima vez que se active el "informe". Por ejemplo, Agua: 100 ml
+    Leche: 50 ml
+    Café: 76 g
+    Dinero: $2.5
+    c. Si el usuario ha ingresado demasiado dinero, la máquina debería ofrecer cambio.
+    Por ejemplo, "Aquí tiene $2.45 de cambio". El cambio debe redondearse a dos decimales.
+
+7. Preparar café.
+    a. Si la transacción es exitosa y hay suficientes recursos para preparar la bebida que el usuario seleccionó, los ingredientes se deducirán de los recursos de la máquina de café.
+    Por ejemplo, informe antes de comprar un café con leche: Agua: 300 ml
+    Leche: 200 ml
+    Café: 100 g
+    Dinero: $0
+    Informe después de comprar un café con leche:
+    Agua: 100 ml
+    Leche: 50 ml
+    Café: 76 g
+    Dinero: $2.5
+    b. Una vez deducidos todos los recursos, diga al usuario: "¡Aquí está su café con leche! ¡Disfrútelo!". Si eligió café con leche.
--- main.py
+++ main.py
@@ -19,7 +19,8 @@ MENU = {
             "agua": 250,
             "leche": 100,
             "cafe": 24
-        }
+        },
+        "costo": 3.0,
     }
 }
 
@@ -31,6 +32,7 @@ recursos = {
 }
 
 def hay_recurso_suficiente(ingredientes_orden):
+    """Retorna True cuando la orden pude ser producida o False si no hay suficientes recursos"""
     es_suficiente = True
     for i in ingredientes_orden:
         if ingredientes_orden[i] >= recursos[i]:
@@ -38,10 +40,38 @@ def hay_recurso_suficiente(ingredientes_orden):
             es_suficiente = False
     return es_suficiente
 
+def proceso_monedas():
+    """Retorna el valor calculado de las monedas insertadas"""
+    print("Por favor inserte las monedas....")
+    total = int(input("¿ Cuantas monedas de $25c ?: ")) * 0.25
+    total += int(input("¿ Cuantas monedas de $10c ?: ")) * 0.10
+    total += int(input("¿ Cuantas monedas de $5c ?: ")) * 0.05
+    total += int(input("¿ Cuantas monedas de $1c ?: ")) * 0.01
+    return total
+
+def transaccion_exitosa(pago, costo_bebida):
+    """Retorna True cuando el pago se hizo exitosamente, o False cuando el pago no ha sido suficiente"""
+    if pago >= costo_bebida:
+        cambio = round(pago - costo_bebida, 2)
+        print(f'Aqui tienes tu cambio ${cambio}')
+        global ganancia
+        ganancia += costo_bebida
+        return True
+    else:
+        print("Lo lamentamos el pago no es suficiente. Te regrasamos tu pago actual!")
+        return False
+
+def hacer_cafe(nombre_bebida, ingredientes):
+    """Deducir los ingredientes requeridos de los recursos"""
+    for ingrediente in ingredientes:
+        recursos[ingrediente] -= ingredientes[ingrediente]
+
+    print(f"Aquí tienes tu café {nombre_bebida} ☕")
+
 switch = True
 
 while switch:
-    eleccion = input("¿Que le gustaria? (espresso, latte, cappucciono): ")
+    eleccion = input("¿Que le gustaria tomar el dia de hoy? (espresso, latte, cappucciono): ")
 
     if eleccion == "off":
         print("📴 Apagando Maquina....")
@@ -53,6 +83,11 @@ while switch:
         print(f'Cafe : ☕ {recursos["cafe"]}g')
         print(f'===========REPORTE GANANCIAS===============')
         print(f'Ganancias 💰: {ganancia}')
-    else:
+    elif eleccion == "latte" or eleccion == "espresso" or eleccion == "cappucciono":
         bebida = MENU[eleccion]
-        #if hay_recurso_suficiente(bebida["ingredientes"]):
+        if hay_recurso_suficiente(bebida["ingredientes"]):
+            pago = proceso_monedas()
+            if transaccion_exitosa(pago, bebida["costo"]):
+                hacer_cafe(eleccion, bebida["ingredientes"])
+    else:
+        print("Lo sentimos no tenemos esa opcion por el momento!")
